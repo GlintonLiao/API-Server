5fdda7f941fe6f637228d5867870ccd977b87f72
Guotong Liao <815190191@qq.com>
2022-01-16 20:32:12 +0800

    update publish article feature

--- router/article.js
+++ router/article.js
@@ -6,12 +6,19 @@ const multer = require('multer')
 // 导入处理路径的核心模块
 const path = require('path')
 
+// 导入验证数据的中间件
+const expressJoi = require('@escook/express-joi')
+// 导入文章的验证模块
+const { add_article_schema } = require('../schema/article')
+
 // 创建 multer 的实例对象，通过 dest 属性指定文件的存放路径
 const upload = multer({dest: path.join(__dirname, '../uploads')})
 
 // 导入文章处理函数
 const article_handler = require('../router_handler/article')
 
-router.post('/add', article_handler.addArticle)
+// 先用 multer 解析表单数据
+// 再用 expressJoi 对解析的表单数据进行验证
+router.post('/add',upload.single('cover_img'),expressJoi(add_article_schema), article_handler.addArticle)
 
 module.exports = router
